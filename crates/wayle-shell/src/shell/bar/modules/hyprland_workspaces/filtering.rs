520c02b89e4425762c4ea0cc8ed16764bf128eb3
TimesZ <crashzeus@protonmail.com>
2026-09-13 10:34:17 +0200

    fix: clippy rust nightly

--- crates/wayle-shell/src/shell/bar/modules/hyprland_workspaces/filtering.rs
+++ crates/wayle-shell/src/shell/bar/modules/hyprland_workspaces/filtering.rs
@@ -1,5 +1,7 @@
-use std::cmp::Ordering;
-use std::collections::{HashMap, HashSet};
+use std::{
+    cmp::Ordering,
+    collections::{HashMap, HashSet},
+};
 
 use super::helpers::{is_special_workspace, matches_ignore_patterns};
 
